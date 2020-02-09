c0016590fd035b26bb97507a996fea32c2d0bc56
Your Name <you@example.com>
2020-02-09 15:33:56 -0300

    .

--- Observers/ClientObserver.php
+++ Observers/ClientObserver.php
@@ -9,9 +9,25 @@ use Modules\Client\Entities\ClientAddress;
 class ClientObserver
 {
 
+	public function creating(Client $client)
+	{
+		if(!is_null($client->cpf_cnpj))
+		{
+			$client->cpf_cnpj = preg_replace('/[^0-9]/', '', $client->cpf_cnpj);
+		}
+	}	
+
 	public function created(Client $client)
 	{
 		ClientAddress::create(['client_id' => $client->id]);
+	}	
+
+	public function updating(Client $client)
+	{
+		if(!is_null($client->cpf_cnpj))
+		{
+			$client->cpf_cnpj = preg_replace('/[^0-9]/', '', $client->cpf_cnpj);
+		}
 	}	
 
 }
