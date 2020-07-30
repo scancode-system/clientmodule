ce2a9b809954145109c81138443355af975054e7
Your Name <you@example.com>
2020-07-30 19:32:03 -0300

    .

--- Providers/ValidationServiceProvider.php
+++ Providers/ValidationServiceProvider.php
@@ -26,47 +26,47 @@ class ValidationServiceProvider extends ServiceProvider
 				$cnpj = $value;
 				for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++)
 				{
-					$soma += $cnpj{$i} * $j;
+					$soma += $cnpj[$i] * $j;
 					$j = ($j == 2) ? 9 : $j - 1;
 				}
 				$resto = $soma % 11;
-				if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto))
+				if ($cnpj[12] != ($resto < 2 ? 0 : 11 - $resto))
 					return false;
 
 				for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++)
 				{
-					$soma += $cnpj{$i} * $j;
+					$soma += $cnpj[$i] * $j;
 					$j = ($j == 2) ? 9 : $j - 1;
 				}
 
 				$resto = $soma % 11;
-				return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
+				return $cnpj[13] == ($resto < 2 ? 0 : 11 - $resto);
 
 			} else if (strlen($value) == 11){
 				$cpf = $value;
 
 				for ($i = 0, $j = 10, $soma = 0; $i < 9; $i++, $j--){
-					$soma += $cpf{$i} * $j;
+					$soma += $cpf[$i] * $j;
 				}
 
 				$resto = $soma % 11;
-				if ($cpf{9} != ($resto < 2 ? 0 : 11 - $resto)){
+				if ($cpf[9] != ($resto < 2 ? 0 : 11 - $resto)){
 					return false;
 				}
 
 				for ($i = 0, $j = 11, $soma = 0; $i < 10; $i++, $j--){
-					$soma += $cpf{$i} * $j;
+					$soma += $cpf[$i] * $j;
 				}
 
 				$resto = $soma % 11;
-				return $cpf{10} == ($resto < 2 ? 0 : 11 - $resto);
+				return $cpf[10] == ($resto < 2 ? 0 : 11 - $resto);
 
 			} else {
 				return false;
 			}
 
 
-			return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto);
+			return $cnpj[13] == ($resto < 2 ? 0 : 11 - $resto);
 			return false;
 		});
 	}
